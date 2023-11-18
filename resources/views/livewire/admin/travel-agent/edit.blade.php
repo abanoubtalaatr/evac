324b37585516ed80b7a409c91ab9812750e13844
abanoubtalaatr <38631117+abanoubtalaatr@users.noreply.github.com>
2023-11-18 22:36:01 +0200

    wip

--- resources/views/livewire/admin/travel-agent/edit.blade.php
+++ resources/views/livewire/admin/travel-agent/edit.blade.php
@@ -4,7 +4,7 @@
             <!-- Add your modal content here -->
             <div class="modal-header">
                 <h5 class="modal-title" id="agentModalLabel{{$agent->id}}">Edit Agent</h5>
-                <button type="button" onclick="$('#agentModal{{$agent->id}}').modal('hide');" class="close btn" data-dismiss="modal" aria-label="Close">
+                <button type="button" onclick="$('#agentModal{{$agent->id}}').modal('hide');" wire:click="emptyForm" class="close btn" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
@@ -13,18 +13,18 @@
             <div class="modal-body">
                 <div class="form-group my-2">
                     <label for="companyName">Name:</label>
-                    <input disabled type="text" wire:model="form.name" class="form-control" id="name">
+                    <input  type="text" wire:model="form.name" class="form-control" id="name">
                     @error('form.name')<p style='color:red'> {{$message}} </p>@enderror
                 </div>
 
                 <div class="form-group my-2">
                     <label for="companyName">Company Name:</label>
-                    <input disabled type="text" wire:model="form.company_name" value="{{$agent->company_name}}" class="form-control" id="companyName">
+                    <input  type="text" wire:model="form.company_name" value="{{$agent->company_name}}" class="form-control" id="companyName">
                     @error('form.company_name')<p style='color:red'> {{$message}} </p>@enderror
                 </div>
                 <div class="form-group my-2">
                     <label for="address">Address : </label>
-                    <input disabled type="text" wire:model="form.address" class="form-control" id="address">
+                    <input  type="text" wire:model="form.address" class="form-control" id="address">
                     @error('form.address')<p style='color:red'> {{$message}} </p>@enderror
                 </div>
 
@@ -42,7 +42,7 @@
 
                 <div class="form-group my-2">
                     <label for="owner_name">Owner name : </label>
-                    <input disabled type="text" wire:model="form.owner_name" class="form-control" id="owner_name">
+                    <input  type="text" wire:model="form.owner_name" class="form-control" id="owner_name">
                     @error('form.owner_name')<p style='color:red'> {{$message}} </p>@enderror
                 </div>
 
@@ -54,27 +54,27 @@
 
                 <div class="form-group my-2">
                     <label for="iata_no">IATA No : </label>
-                    <input disabled type="text" wire:model="form.iata_no" class="form-control" id="iata_no">
+                    <input  type="text" wire:model="form.iata_no" class="form-control" id="iata_no">
                     @error('form.iata_no')<p style='color:red'> {{$message}} </p>@enderror
                 </div>
 
                 <div class="form-group my-2">
                     <label for="finance_no">Finance No : </label>
-                    <input disabled type="text" wire:model="form.finance_no" class="form-control" id="finance_no">
+                    <input  type="text" wire:model="form.finance_no" class="form-control" id="finance_no">
                     @error('form.finance_no')<p style='color:red'> {{$message}} </p>@enderror
                 </div>
 
 
                 <div class="form-group my-2">
                     <label for="telephone">Telephone:</label>
-                    <input disabled type="tel" wire:model="form.telephone" class="form-control" id="telephone">
+                    <input  type="tel" wire:model="form.telephone" class="form-control" id="telephone">
                     @error('form.telephone')<p style='color:red'> {{$message}} </p>@enderror
                 </div>
                 <!-- Add more form fields as needed -->
             </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" onclick="$('#agentModal{{$agent->id}}').modal('hide');" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" onclick="$('#agentModal{{$agent->id}}').modal('hide');" wire:click="emptyForm" data-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary" wire:click="update">Update</button>
 
                 </div>
