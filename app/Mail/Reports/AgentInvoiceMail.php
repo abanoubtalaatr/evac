53ea05b2d3ca6ff65d6783404014403b66097628
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-02-21 14:48:34 +0200

    WIP

--- app/Mail/Reports/AgentInvoiceMail.php
+++ app/Mail/Reports/AgentInvoiceMail.php
@@ -23,9 +23,9 @@ class AgentInvoiceMail extends Mailable
 
     public function build()
     {
-        $name = $this->agent ? $this->agent->name.'_invoice.pdf':"agent_invoice.pdf";
+        $name = $this->agent ? $this->agent->name.'_INVOICE.pdf':"agent_INVOICE.pdf";
         return $this->attachData($this->generatePdf(), $name)
-            ->subject("EVAC - "  . $name. " Invoice")
+            ->subject("EVAC - "  . $name. " INVOICE")
             ->view('emails.TravelAgent.agent-applications-body',[
                 'agentInvoice' => true
             ]);
