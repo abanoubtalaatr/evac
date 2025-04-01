5a5f9dda4c81a872fc4ea3cdb4af147c5eeb358f
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-04-01 21:36:56 +0200

    check git flow

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -5,13 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Service extends Model{
+class Service extends Model
+{
     use HasFactory;
+    
     protected $guarded=[];
 
     public function serviceTransactions()
     {
         return $this->hasMany(ServiceTransaction::class);
     }
-    
+   
 }
