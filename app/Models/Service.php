43e1b1a94bb88685035fcbad12fdfbce9203a33b
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-04-01 21:41:16 +0200

    testing gitflow

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Service extends Model{
+class Service extends Model
+{
     use HasFactory;
+    
     protected $guarded=[];
 
     public function serviceTransactions()
